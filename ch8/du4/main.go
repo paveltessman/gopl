f284d883da9ce998429bb9d1237ede662f5b4e6f
Pavel <tessmanpavel@gmail.com>
2026-09-25 14:43:25 +0300

    ch8: make du4

---
+++ ch8/du4/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+var verbose = flag.Bool("v", false, "show verbose progress messages")
+
+var sema = make(chan struct{}, 20)
+
+var done = make(chan struct{})
+
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
+	}
+}
+
+func dirents(dir string) []os.DirEntry {
+
+	select {
+	case sema <- struct{}{}:
+	case <-done:
+		return nil
+	}
+
+	defer func() { <-sema }()
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du1: %v\n", err)
+		return nil
+	}
+	return entries
+}
+
+func walkDir(dir string, wg *sync.WaitGroup, fileSizes chan<- int64) {
+
+	defer wg.Done()
+
+	if cancelled() {
+		return
+	}
+
+	for _, entry := range dirents(dir) {
+
+		info, err := entry.Info()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "du1: %v\n", err)
+		}
+
+		if !info.IsDir() {
+			fileSizes <- info.Size()
+			continue
+		}
+
+		wg.Add(1)
+		subdir := filepath.Join(dir, info.Name())
+		go walkDir(subdir, wg, fileSizes)
+	}
+}
+
+func printDiskUsage(nfiles, nbytes int64) {
+	fmt.Printf("\r%d files %.1f GB", nfiles, float64(nbytes)/1e9)
+}
+
+func main() {
+
+	flag.Parse()
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	fileSizes := make(chan int64)
+
+	var wg sync.WaitGroup
+
+	for _, root := range roots {
+		wg.Add(1)
+		go walkDir(root, &wg, fileSizes)
+	}
+
+	go func() {
+		os.Stdin.Read(make([]byte, 1))
+		close(done)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(fileSizes)
+	}()
+
+	var tick <-chan time.Time
+	if *verbose {
+		tick = time.Tick(500 * time.Millisecond)
+	}
+
+	var nfiles, nbytes int64
+
+loop:
+	for {
+		select {
+
+		case <-done:
+			for range fileSizes {
+				// drain
+			}
+
+		case size, ok := <-fileSizes:
+			if !ok {
+				break loop
+			}
+			nfiles++
+			nbytes += size
+
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
+
+		}
+	}
+
+	printDiskUsage(nfiles, nbytes)
+	fmt.Println()
+
+}
